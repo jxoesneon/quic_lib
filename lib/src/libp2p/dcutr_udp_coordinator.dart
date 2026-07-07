f150c4d4618284c42203c3069f634d574440c53b
Eduardo Rojas <joseeduardox@gmail.com>
2026-07-07 13:01:57 -0600

    docs(libp2p, http3): document DCUtR coordinator and WebTransport connect request
    - Add dartdoc to DCUtRUdpCoordinator constructor.
    - Document WebTransportConnectRequest fields and constructor.

--- lib/src/libp2p/dcutr_udp_coordinator.dart
+++ lib/src/libp2p/dcutr_udp_coordinator.dart
@@ -20,6 +20,7 @@ class DCUtRUdpCoordinator {
   StreamSubscription<({Uint8List data, InternetAddress address, int port})>?
       _subscription;
 
+  /// Creates a coordinator that drives DCUtR over [socket] using [stateMachine].
   DCUtRUdpCoordinator(this._socket, this._stateMachine);
 
   /// Starts listening to the underlying [UdpSocket] for DCUtR datagrams.
